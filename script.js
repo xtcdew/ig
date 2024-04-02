625567de82feaeca36de298aea6e4e93deddefeb
xtcdew <79530832+xtcdew@users.noreply.github.com>
2024-04-02 10:30:58 +0600

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ showTime();
 setInterval(function () {
 	showTime();
 }, 1000);
+
+document.addEventListener("DOMContentLoaded", function() {
+  const button = document.querySelector(".btn");
+  const image = document.querySelector(".image-container69 img");
+  
+  button.addEventListener("click", function() {
+    alert("You clicked the button!");
+  });
+  
+  image.addEventListener("mouseenter", function() {
+    this.style.filter = "grayscale(100%)";
+  });
+  
+  image.addEventListener("mouseleave", function() {
+    this.style.filter = "none";
+  });
+});
